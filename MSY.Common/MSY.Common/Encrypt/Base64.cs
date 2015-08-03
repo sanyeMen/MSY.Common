4fba5b153851d7f6c2701df5b74b7fa933c543cc
sanyeMen <liu610721@gmail.com>
2015-08-03 10:54:14 +0800

    添加带代码文件

---
+++ MSY.Common/MSY.Common/Encrypt/Base64.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSY.Common.Encrypt
+{
+    /// <summary>
+    /// 提供Base64加密和解密方法
+    /// </summary>
+    public sealed class Base64
+    {
+        /// <summary>
+        /// Base64加密，不带编码，使用系统默认编码
+        /// </summary>
+        /// <param name="input">需要加密的字符串</param>
+        /// <returns></returns>
+        public static string Base64Encrypt(string input)
+        {
+            return Base64Encrypt(input, Encoding.Default);
+        }
+
+        /// <summary>
+        /// Base64加密，带编码
+        /// </summary>
+        /// <param name="input">需要加密的字符串</param>
+        /// <param name="encode">字符集编码</param>
+        /// <returns>如果需要加密的字符串是null，返回null</returns>
+        public static string Base64Encrypt(string input, Encoding encode)
+        {
+            if (input == null)
+                return null;
+
+            if (encode == null)
+                encode = Encoding.Default;
+
+            return Convert.ToBase64String(encode.GetBytes(input));
+        }
+
+        /// <summary>
+        /// Base64解密，不带编码，使用系统默认编码
+        /// </summary>
+        /// <param name="input">需要解密的字符串</param>
+        /// <returns></returns>
+        public static string Base64Decrypt(string input)
+        {
+            return Base64Decrypt(input, Encoding.Default);
+        }
+
+        /// <summary>
+        /// Base64解密，带编码
+        /// </summary>
+        /// <param name="input">需要解密的字符串</param>
+        /// <param name="encode">字符集编码</param>
+        /// <returns>如果需要解密的字符串是null，返回null</returns>
+        public static string Base64Decrypt(string input, Encoding encode)
+        {
+            if (input == null)
+                return null;
+
+            if(encode == null)
+                encode = Encoding.Default;
+
+            return encode.GetString(Convert.FromBase64String(input));
+        }
+    }
+}
---
+++ MSY.Common/MSY.Common/Encrypt/DES.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSY.Common.Encrypt
+{
+    /// <summary>
+    /// 提供DES/3DES加密和解密
+    /// </summary>
+    public sealed class DES
+    {
+        /// <summary>
+        /// DES加密
+        /// </summary>
+        /// <param name="data">加密数据</param>
+        /// <param name="key">8位字符的密钥字符串</param>
+        /// <param name="iv">8位字符的初始化向量字符串</param>
+        /// <returns></returns>
+        public static string DESEncrypt(string data, string key, string iv)
+        {
+            byte[] byKey = ASCIIEncoding.ASCII.GetBytes(key);
+            byte[] byIV = ASCIIEncoding.ASCII.GetBytes(iv);
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cst = new CryptoStream(ms, cryptoProvider.CreateEncryptor(byKey, byIV), CryptoStreamMode.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(cst))
+                    {
+                        sw.Write(data);
+                    }
+                }
+
+                return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+            }
+        }
+
+        /// <summary>
+        /// DES解密
+        /// </summary>
+        /// <param name="data">解密数据</param>
+        /// <param name="key">8位字符的密钥字符串(需要和加密时相同)</param>
+        /// <param name="iv">8位字符的初始化向量字符串(需要和加密时相同)</param>
+        /// <returns></returns>
+        public static string DESDecrypt(string data, string key, string iv)
+        {
+            byte[] byKey = ASCIIEncoding.ASCII.GetBytes(key);
+            byte[] byIV = ASCIIEncoding.ASCII.GetBytes(iv);
+
+            byte[] byEnc;
+            try
+            {
+                byEnc = Convert.FromBase64String(data);
+            }
+            catch
+            {
+                return null;
+            }
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+
+            using (MemoryStream ms = new MemoryStream(byEnc))
+            {
+                using (CryptoStream cst = new CryptoStream(ms, cryptoProvider.CreateDecryptor(byKey, byIV), CryptoStreamMode.Read))
+                {
+                    using (StreamReader sr = new StreamReader(cst))
+                    {
+                        return sr.ReadToEnd();           
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 3DES加密
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string DES3Encrypt(string data, string key)
+        {
+            TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            DES.Mode = CipherMode.CBC;
+            DES.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform DESEncrypt = DES.CreateEncryptor();
+
+            byte[] Buffer = ASCIIEncoding.ASCII.GetBytes(data);
+            return Convert.ToBase64String(DESEncrypt.TransformFinalBlock(Buffer, 0, Buffer.Length));
+        }
+
+        /// <summary>
+        /// 3DES解密
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string DES3Decrypt(string data, string key)
+        {
+            TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            DES.Mode = CipherMode.CBC;
+            DES.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform DESDecrypt = DES.CreateDecryptor();
+
+            string result = "";
+            try
+            {
+                byte[] Buffer = Convert.FromBase64String(data);
+                result = ASCIIEncoding.ASCII.GetString(DESDecrypt.TransformFinalBlock(Buffer, 0, Buffer.Length));
+            }
+            catch (Exception e)
+            {
+
+            }
+            return result;
+        }
+
+    }
+}
